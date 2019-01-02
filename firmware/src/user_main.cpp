b9f8addff846ff74e0484d573069624bdfe25288
Piroro-hs <Piroro-hs@users.noreply.github.com>
2019-01-03 01:54:27 +0900

    Configure USART

--- firmware/src/user_main.cpp
+++ firmware/src/user_main.cpp
@@ -1,11 +1,15 @@
 #include "user_main.h"
 
+#include <stdio.h>
+
+#include "stm32f303x8.h"
 #include "stm32f3xx_hal.h"
 #include "stm32f3xx_hal_gpio.h"
 
 void setup() {}
 
 void loop() {
+  printf("test\n");
   HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
   HAL_Delay(100);
 }

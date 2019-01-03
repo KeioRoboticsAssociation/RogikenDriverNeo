f7025c6e26f6791131fb602b205b08bfc8fcaf92
Piroro-hs <Piroro-hs@users.noreply.github.com>
2019-01-04 02:17:52 +0900

    Use TIM3 in Encoder Mode

--- firmware/src/user_main.cpp
+++ firmware/src/user_main.cpp
@@ -4,23 +4,23 @@
 #include <stddef.h>
 #include <stdio.h>
 
-#include "stm32f303x8.h"
 #include "stm32f3xx_hal.h"
-#include "stm32f3xx_hal_gpio.h"
 
 namespace {
   constexpr size_t UART_RX_DMA_CIRC_BUF_SIZE = 64;
   uint8_t uart_rx_dma_circ_buf[UART_RX_DMA_CIRC_BUF_SIZE];
 }
 
+extern TIM_HandleTypeDef htim3;
 extern UART_HandleTypeDef huart2;
 
 void setup() {
+  HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
   HAL_UART_Receive_DMA(&huart2, uart_rx_dma_circ_buf, UART_RX_DMA_CIRC_BUF_SIZE);
 }
 
 void loop() {
-  printf("%s %" PRIu32 "\n", uart_rx_dma_circ_buf, huart2.hdmarx->Instance->CNDTR);
+  printf("%s %" PRIu32 " %" PRIu32 "\n", uart_rx_dma_circ_buf, huart2.hdmarx->Instance->CNDTR, TIM3->CNT);
   HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
   HAL_Delay(100);
 }

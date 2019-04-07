4e5faf1c05c77ab1f73781006b057a9ffefb3062
Piroro-hs <Piroro-hs@users.noreply.github.com>
2019-04-07 12:08:16 +0900

    Lift velocity limit

--- firmware/include/pid_task.h
+++ firmware/include/pid_task.h
@@ -11,7 +11,7 @@ class PidTask : public Task {
   private:
     static constexpr float ZERO = 0;
     static constexpr float ACC_LIM = 1;
-    static constexpr float VEL_LIM = 30;
+    static constexpr float VEL_LIM = 100;
     arm_pid_instance_f32 pid;
     const float* input = &ZERO;
     const float* set_point = &ZERO;
